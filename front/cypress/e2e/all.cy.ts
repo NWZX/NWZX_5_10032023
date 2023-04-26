b29ce30870c17c6647933a9119a38c605fae0c3c
NWZX <nwzx@users.noreply.github.com>
2023-04-26 10:17:06 +0200

    optional simulate e2e

--- front/cypress/e2e/all.cy.ts
+++ front/cypress/e2e/all.cy.ts
@@ -6,18 +6,9 @@ describe('All spec', () => {
         cy.visit('/random');
         cy.url().should('include', '/404');
     });
-    it('Register successfully', () => {
+  it('Register successfully', () => {
+        
         cy.visit('/register');
-
-        cy.intercept('POST', '/api/auth/register', {
-        body: {
-            id: 1,
-            email: 'userName',
-            firstName: 'firstName',
-            lastName: 'lastName',
-            admin: true,
-        },
-        });
 
         cy.intercept(
         {
@@ -29,7 +20,9 @@ describe('All spec', () => {
 
         cy.get('input[formControlName=firstName]').type('yoga');
         cy.get('input[formControlName=lastName]').type('studio');
-        cy.get('input[formControlName=email]').type('yoga@studio.com');
+        cy.get('input[formControlName=email]').type(
+          `yoga${Date.now().toString()}@studio.com`
+        );
         cy.get('input[formControlName=password]').type(
         `${'test!1234'}{enter}{enter}`
         );
@@ -49,157 +42,29 @@ describe('All spec', () => {
       cy.get('.mat-card-title', { multiple: true }).should('have.length', 3); // 2 sessions from the mock + 1 from the "Create new session" card
     });
     it('See account info successfully', () => {
-      cy.intercept('GET', '/api/user/1', {
-        body: {
-          id: 1,
-          email: 'yoga@studio.com',
-          firstName: 'yoga',
-          lastName: 'studio',
-          admin: true,
-          createdAt: '2021-05-05T12:00:00.000Z',
-          updatedAt: '2021-05-05T12:00:00.000Z',
-        },
-      }).as('me');
-
       cy.get('#account').click();
 
       cy.url().should('include', '/me');
 
-      cy.get('p').contains('Name:').should('have.text', 'Name: yoga STUDIO');
+      cy.get('p').contains('Name:').should('have.text', 'Name: Admin ADMIN');
     });
     it('Show detail successfully', () => {
-      cy.intercept(
-        {
-          method: 'GET',
-          url: '/api/session/1',
-        },
-        {
-          id: 1,
-          name: 'Session 1',
-          date: '2023-04-01T09:00:00Z',
-          teacher_id: 1234,
-          description: 'This is a test session.',
-          users: [5678, 9012],
-          createdAt: '2023-03-20T12:34:56.789Z',
-          updatedAt: '2023-03-20T12:34:56.789Z',
-        }
-      ).as('session');
-      cy.intercept(
-        {
-          method: 'GET',
-          url: '/api/teacher/1234',
-        },
-        {
-          id: 1,
-          lastName: 'Tatcher',
-          firstName: 'Maggy',
-          createdAt: '2023-03-20T12:34:56.789Z',
-          updatedAt: '2023-03-20T12:34:56.789Z',
-        }
-      ).as('teacher');
-
       cy.get('.mat-card-title', { multiple: true }).should('have.length', 3);
       cy.get('button.mat-focus-indicator').eq(1).click();
       cy.url().should('include', '/detail/1');
-      cy.get('h1').contains('Session 1');
+      cy.get('h1').contains('Dance');
     });
     it('Show edit successfully', () => {
-      cy.intercept(
-        {
-          method: 'GET',
-          url: '/api/session/1',
-        },
-        {
-          id: 1,
-          name: 'Session 1',
-          date: '2023-04-01T09:00:00Z',
-          teacher_id: 1234,
-          description: 'This is a test session.',
-          users: [5678, 9012],
-          createdAt: '2023-03-20T12:34:56.789Z',
-          updatedAt: '2023-03-20T12:34:56.789Z',
-        }
-      ).as('session');
-      cy.intercept(
-        {
-          method: 'GET',
-          url: '/api/teacher',
-        },
-        [
-          {
-            id: 1234,
-            lastName: 'Tatcher',
-            firstName: 'Maggy',
-            createdAt: '2023-03-20T12:34:56.789Z',
-            updatedAt: '2023-03-20T12:34:56.789Z',
-          },
-          {
-            id: 1235,
-            lastName: 'Abyss',
-            firstName: 'Aby',
-            createdAt: '2023-03-20T12:34:56.789Z',
-            updatedAt: '2023-03-20T12:34:56.789Z',
-          },
-        ]
-      ).as('teacher');
-      cy.intercept('PUT', '/api/session/1', {
-        body: {
-          id: 1,
-          name: 'Session 5',
-          date: '2023-04-01T09:00:00Z',
-          teacher_id: 1234,
-          description: 'This is a test session.',
-          users: [5678, 9012],
-          createdAt: '2023-03-20T12:34:56.789Z',
-          updatedAt: '2023-03-20T12:34:56.789Z',
-        },
-      });
-
       cy.get('.mat-card-title', { multiple: true }).should('have.length', 3);
       cy.get('button.mat-focus-indicator').eq(2).click();
       cy.url().should('include', '/update/1');
       cy.get('h1').contains('Update session');
-      cy.get('#mat-input-2').should('have.value', 'Session 1');
-      cy.get('#mat-input-2').clear().type('Session 5');
+      cy.get('#mat-input-2').should('have.value', 'Dance');
+      cy.get('#mat-input-2').clear().type('Dance');
       cy.get('button[type=submit]').click();
       cy.url().should('include', '/sessions');
     });
     it('Show create successfully', () => {
-      cy.intercept(
-        {
-          method: 'GET',
-          url: '/api/teacher',
-        },
-        [
-          {
-            id: 1234,
-            lastName: 'Tatcher',
-            firstName: 'Maggy',
-            createdAt: '2023-03-20T12:34:56.789Z',
-            updatedAt: '2023-03-20T12:34:56.789Z',
-          },
-          {
-            id: 1235,
-            lastName: 'Abyss',
-            firstName: 'Aby',
-            createdAt: '2023-03-20T12:34:56.789Z',
-            updatedAt: '2023-03-20T12:34:56.789Z',
-          },
-        ]
-      ).as('teacher');
-      cy.intercept('POST', '/api/session', {
-        body: {
-          id: 1,
-          name: 'Session 5',
-          date: '2023-04-01T09:00:00Z',
-          teacher_id: 1234,
-          description: 'This is a test session.',
-          users: [5678, 9012],
-          createdAt: '2023-03-20T12:34:56.789Z',
-          updatedAt: '2023-03-20T12:34:56.789Z',
-        },
-      });
-
       cy.get('.mat-card-title', { multiple: true }).should('have.length', 3);
       cy.get('button.mat-focus-indicator').eq(0).click();
       cy.url().should('include', '/sessions/create');
@@ -210,6 +75,13 @@ describe('All spec', () => {
       cy.get('#mat-option-1').click();
       cy.get('#mat-input-4').clear().type('This is a test session.');
       cy.get('button[type=submit]').click();
+      cy.url().should('include', '/sessions');
+    });
+    it('Show delete successfully', () => {
+      cy.get('.mat-card-title', { multiple: true }).should('have.length', 4);
+      cy.get('button.mat-focus-indicator').eq(5).click();
+      cy.get('h1').contains('Session 5');
+      cy.get('button.mat-focus-indicator').eq(1).click();
       cy.url().should('include', '/sessions');
     });
 });
--- front/cypress/support/commands.ts
+++ front/cypress/support/commands.ts
@@ -4,25 +4,71 @@
 // IDE or Text Editor.
 // ***********************************************
 
-const login = (email: string, password: string) => {
+const login = (email: string, password: string, simultate: boolean = false) => {
   cy.visit('/login');
-
-  cy.intercept('POST', '/api/auth/login', {
-    body: {
-      id: 1,
-      username: 'userName',
-      firstName: 'firstName',
-      lastName: 'lastName',
-      admin: true,
-    },
-  });
-
-  cy.intercept(
-    {
-      method: 'GET',
-      url: '/api/session',
-    },
-    [
+  if (simultate) {
+    cy.intercept('POST', '/api/auth/login', {
+      body: {
+        id: 1,
+        username: 'userName',
+        firstName: 'firstName',
+        lastName: 'lastName',
+        admin: true,
+      },
+    });
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/api/session',
+      },
+      [
+        {
+          id: 1,
+          name: 'Session 1',
+          date: '2023-04-01T09:00:00Z',
+          teacher_id: 1234,
+          description: 'This is a test session.',
+          users: [5678, 9012],
+          createdAt: '2023-03-20T12:34:56.789Z',
+          updatedAt: '2023-03-20T12:34:56.789Z',
+        },
+        {
+          id: 2,
+          name: 'Session 2',
+          date: '2023-04-02T10:00:00Z',
+          teacher_id: 5678,
+          description: 'This is another test session.',
+          users: [1234],
+          createdAt: '2023-03-21T13:45:00.123Z',
+          updatedAt: '2023-03-22T14:56:00.234Z',
+        },
+      ]
+    ).as('session');
+    cy.intercept('POST', '/api/auth/register', {
+      body: {
+        id: 1,
+        email: 'userName',
+        firstName: 'firstName',
+        lastName: 'lastName',
+        admin: true,
+      },
+    });
+    cy.intercept('GET', '/api/user/1', {
+      body: {
+        id: 1,
+        email: 'yoga@studio.com',
+        firstName: 'admin',
+        lastName: 'ADMIN',
+        admin: true,
+        createdAt: '2021-05-05T12:00:00.000Z',
+        updatedAt: '2021-05-05T12:00:00.000Z',
+      },
+    }).as('me');
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/api/session/1',
+      },
       {
         id: 1,
         name: 'Session 1',
@@ -32,20 +78,68 @@ const login = (email: string, password: string) => {
         users: [5678, 9012],
         createdAt: '2023-03-20T12:34:56.789Z',
         updatedAt: '2023-03-20T12:34:56.789Z',
+      }
+    ).as('sessionOne');
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/api/teacher',
+      },
+      [
+        {
+          id: 1234,
+          lastName: 'Tatcher',
+          firstName: 'Maggy',
+          createdAt: '2023-03-20T12:34:56.789Z',
+          updatedAt: '2023-03-20T12:34:56.789Z',
+        },
+        {
+          id: 1235,
+          lastName: 'Abyss',
+          firstName: 'Aby',
+          createdAt: '2023-03-20T12:34:56.789Z',
+          updatedAt: '2023-03-20T12:34:56.789Z',
+        },
+      ]
+    ).as('teacher');
+    cy.intercept('PUT', '/api/session/1', {
+      body: {
+        id: 1,
+        name: 'Session 5',
+        date: '2023-04-01T09:00:00Z',
+        teacher_id: 1234,
+        description: 'This is a test session.',
+        users: [5678, 9012],
+        createdAt: '2023-03-20T12:34:56.789Z',
+        updatedAt: '2023-03-20T12:34:56.789Z',
+      },
+    });
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/api/teacher/1234',
       },
       {
-        id: 2,
-        name: 'Session 2',
-        date: '2023-04-02T10:00:00Z',
-        teacher_id: 5678,
-        description: 'This is another test session.',
-        users: [1234],
-        createdAt: '2023-03-21T13:45:00.123Z',
-        updatedAt: '2023-03-22T14:56:00.234Z',
+        id: 1,
+        lastName: 'Tatcher',
+        firstName: 'Maggy',
+        createdAt: '2023-03-20T12:34:56.789Z',
+        updatedAt: '2023-03-20T12:34:56.789Z',
+      }
+    ).as('teacherOne');
+    cy.intercept('POST', '/api/session', {
+      body: {
+        id: 1,
+        name: 'Session 5',
+        date: '2023-04-01T09:00:00Z',
+        teacher_id: 1234,
+        description: 'This is a test session.',
+        users: [5678, 9012],
+        createdAt: '2023-03-20T12:34:56.789Z',
+        updatedAt: '2023-03-20T12:34:56.789Z',
       },
-    ]
-  ).as('session');
-
+    });
+  }
   cy.get('input[formControlName=email]').type(email);
   cy.get('input[formControlName=password]').type(`${password}{enter}{enter}`);
 };
